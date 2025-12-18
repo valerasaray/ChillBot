3d699ad3ba256fee32609dfe18d209306cf5a095
valerasaray <YOUR_EMAIL>
2025-12-19 00:19:50 +0300

    api

--- src/chillbot_api/services/llm/abstract_llm_client.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractLlmClient(ABC):
-    @abstractmethod
-    def invoke(self, message: str) -> str:
-        pass
-    
--- src/chillbot_api/services/logger/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - [%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
-)
-
-logger = logging.getLogger(__name__)
-
